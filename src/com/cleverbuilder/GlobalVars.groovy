030e7b00e326ab2eafd73f70c27e44865c529a08
goyalvickey78 <53388335+goyalvickey78@users.noreply.github.com>
2019-09-23 17:12:44 +0530

    Update GlobalVars.groovy

--- src/com/cleverbuilder/GlobalVars.groovy
+++ src/com/cleverbuilder/GlobalVars.groovy
@@ -3,7 +3,7 @@ package com.cleverbuilder
 
 class GlobalVars {
    static String foo = "bar"
-
+   echo "This script is running from MPL."
    // refer to this in a pipeline using:
    //
    // import com.cleverbuilder.GlobalVars
